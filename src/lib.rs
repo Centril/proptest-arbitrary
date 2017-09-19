46ecf10f005a1297c27b303255cd67ae6a20ab98
Mazdak <twingoow@gmail.com>
2017-09-19 08:48:44 +0200

    added Arbitrary implementations for newtype Bits (working with proptest::bits).

--- src/lib.rs
+++ src/lib.rs
@@ -407,6 +407,75 @@ impl_arbitrary!(String, RegexGeneratorStrategy<String>, {
 });
 
 //==============================================================================
+// Bits:
+//==============================================================================
+
+use std::marker::PhantomData;
+
+use proptest::bits::{self, BitSetLike, BitSetStrategy};
+use proptest::strategy::statics::{Map, MapFn};
+
+/// Bits is a simple newtype for treating the generic type parameter `T` as
+/// a set of bits for the purposes of production of arbitrary values.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct Bits<T: BitSetLike>(T);
+
+impl<T: BitSetLike> From<T> for Bits<T> {
+    fn from(x: T) -> Self {
+        Bits(x)
+    }
+}
+
+/// Do **NOT** use this type directly. This is a private implementation detail
+/// that is unfortunately leaking, which might change in the future.
+/// No guarantees are made regarding the stability of this type.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct FromMapper<I, O>(PhantomData<(I, O)>);
+
+impl<I, O> Default for FromMapper<I, O> {
+    fn default() -> Self {
+        FromMapper(PhantomData)
+    }
+}
+
+impl<I, O: From<I> + Debug> MapFn<I> for FromMapper<I, O> {
+    type Output = O;
+
+    fn apply(&self, val: I) -> Self::Output {
+        val.into()
+    }
+}
+
+macro_rules! impl_bits {
+    ($([$typ: ty => $arb_strat_val: expr]),*) => {
+        $(
+            impl From<Bits<Self>> for $typ {
+                fn from(x: Bits<Self>) -> Self { x.0 }
+            }
+
+            impl<'a> Arbitrary<'a> for Bits<$typ> {
+                valuetree!();
+                type Strategy = Map<BitSetStrategy<$typ>, FromMapper<$typ, Self>>;
+                fn arbitrary() -> Self::Strategy {
+                    Map::new($arb_strat_val, FromMapper::default())
+                }
+            }
+        )*
+    };
+}
+
+impl_bits!([i8 => bits::i8::ANY],
+           [i16 => bits::i16::ANY],
+           [i32 => bits::i32::ANY],
+           [i64 => bits::i64::ANY],
+           [isize => bits::isize::masked(!0isize)],
+           [u8 =>  bits::u8::ANY],
+           [u16 => bits::u16::ANY],
+           [u32 => bits::u32::ANY],
+           [u64 => bits::u64::ANY],
+           [usize => bits::usize::masked(!0usize)]);
+
+//==============================================================================
 // Tuples:
 //==============================================================================
 
